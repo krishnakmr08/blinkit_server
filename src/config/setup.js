84d75f1467f1b199dae4768029cb4a65ad16f69d
krishna kumar <krishnakumar@krishnas-Mac-mini.local>
2025-08-07 01:55:42 +0530

    setup MongoDB connection and .env config

---
+++ src/config/setup.js
@@ -0,0 +1,55 @@
+import AdminJS from "adminjs";
+import AdminJSFastify from "@adminjs/fastify";
+import * as AdminJSMongoose from "@adminjs/mongoose";
+import * as Models from "../models/index.js";
+import { authenticate, COOKIE_PASSWORD, sessionStore } from "./config.js";
+import { dark, light, noSidebar } from "@adminjs/themes";
+
+AdminJS.registerAdapter(AdminJSMongoose);
+
+export const admin = new AdminJS({
+  resources: [
+    {
+      resource: Models.Customer,
+      options: {
+        listProperties: ["Phone", "role", "isActivated"],
+        filterProperties: ["Phone", "role"],
+      },
+    },
+    { resource: Models.Branch },
+    { resource: Models.Product },
+    { resource: Models.Category },
+    { resource: Models.Order },
+    { resource: Models.Counter },
+    { resource: Models.DeliveryPartner },
+    { resource: Models.Admin },
+  ],
+  branding: {
+    companyName: "Blinkit",
+    withMadeWithLove: false,
+  },
+  defaultTheme: dark.id,
+  availableThemes: [dark, light, noSidebar],
+  rootPath: "/admin",
+});
+
+export const buildAdminRouter = async (app) => {
+  await AdminJSFastify.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate,
+      cookiePassword: COOKIE_PASSWORD,
+      cookieName: "adminjs",
+    },
+    app,
+    {
+      store: sessionStore,
+      saveUnitialized: true,
+      secret: COOKIE_PASSWORD,
+      cookie: {
+        httpOnly: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production",
+      },
+    }
+  );
+};
